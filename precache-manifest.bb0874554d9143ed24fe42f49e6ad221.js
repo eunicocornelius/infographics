713c461769ef9a1740179a775c8204745972f967
Nico <eunico.cornelius@gmail.com>
2020-10-01 17:51:04 +0700

    Updates

---
+++ precache-manifest.bb0874554d9143ed24fe42f49e6ad221.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86812884380aa77ec01def27e93ff5d7",
+    "url": "/infographics/index.html"
+  },
+  {
+    "revision": "60b0b6d4a753f741a938",
+    "url": "/infographics/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e62232a3d07fa8fe9d3d",
+    "url": "/infographics/static/js/2.86624015.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infographics/static/js/2.86624015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60b0b6d4a753f741a938",
+    "url": "/infographics/static/js/main.bb9e9ffb.chunk.js"
+  },
+  {
+    "revision": "20eb046b56acb6c442ca",
+    "url": "/infographics/static/js/runtime-main.2a4a3b5f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/infographics/static/media/logo.5d5d9eef.svg"
+  }
+]);
