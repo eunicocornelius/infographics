9a1a5fa8e28fab29afa8f2be16555783ed893a8a
Nico <eunico.cornelius@gmail.com>
2020-10-06 18:04:41 +0700

    added fetch api and layouting using material-ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import personalData from "./personal_data.json";
+import { Grid } from "@material-ui/core";
+import PplCard from "./components/PplCard";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    //async function to fetch data from API
+    async function fetchData() {
+      setUsers(
+        //await helps for getting the data first before continuing to next process the page
+        await fetch("https://reqres.in/api/users?page=2")
+          .then((res) => res.json())
+          .then((res) => res.data)
+          .catch((err) => console.log(err, "Fetch Warning!!"))
+      );
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h3>Responsive Material UI Layout</h3>
+      <Grid container spacing={10} style={{ padding: "24px" }}>
+        {users.map((user) => (
+          <Grid key={user.id} item xs={12} sm={6} md={4} lg={4} xl={3}>
+            <PplCard
+              key={user.id}
+              email={user.email}
+              firstname={user.first_name}
+              lastname={user.last_name}
+              avatar={user.avatar}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 }
